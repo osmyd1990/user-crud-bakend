5f0bf15f6c1d7d47bcf1b4c25b2ae606134e7abe
osmar <osmy19@gmail.com>
2023-04-09 23:33:25 -0500

    error letter

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ const config = {
       dialect: "postgres",
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      database: process.env.DBD_NAME,
+      database: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       //Extra configs
