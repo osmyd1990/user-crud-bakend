86cb1df696243a39e2ed8adbb3ff9f50da35c24e
osmyd1990 <osmyd1990@gmail.com>
2023-05-06 00:52:02 -0500

    debug error

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const db = require('./utils/database')
 const initModels = require('./models/initModels')
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 //? Validar la conexión 
 
